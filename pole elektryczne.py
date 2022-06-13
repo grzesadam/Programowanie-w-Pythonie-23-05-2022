d5d8e27b3cad8747c61bfb166fd070da8fdd8a2d
aboliq <ab301978@student.polsl.pl>
2022-06-13 13:13:11 +0200

    zajecia 13.06

--- pole elektryczne.py
+++ pole elektryczne.py
@@ -3,19 +3,6 @@ from matplotlib import cm
 import numpy as np
 import random
 
-axes = [10, 10, 10]
-
-dane_test = np.ones(axes)
-
-alpha = 0.3
-colors = np.empty(axes + [4], dtype=np.float32)
-colors = [1, 1, 1, alpha]
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-ax.voxels(dane_test, facecolors=colors)
-
 q = []
 i = 0
 Z = 2
@@ -31,18 +18,27 @@ while i < 10:
 print("q", q)
 
 wspolrzedne = []
+x_ujemne = []
+y_ujemne = []
+z_ujemne = []
 
+x_dodatnie = []
+y_dodatnie = []
+z_dodatnie = []
 for j in range(len(q)):
     x = (random.uniform(0, 10))
     y = (random.uniform(0, 10))
     z = (random.uniform(0, 10))
     wspolrzedne.append((x, y, z))
     if q[j] < 0:
-        ax.scatter(x, y, z, c='blue')
+        x_ujemne.append(x)
+        y_ujemne.append(y)
+        z_ujemne.append(z)
     else:
-        ax.scatter(x, y, z, c='red')
+        x_dodatnie.append(x)
+        y_dodatnie.append(y)
+        z_dodatnie.append(z)
 print(wspolrzedne)
-
 
 k = 9 * 10 ** 9
 
@@ -63,28 +59,24 @@ def potential():
 
 print(potential())
 
-# fig = plt.figure(figsize=plt.figaspect(2.))
-# fig.suptitle('Rozmieszczenie ładunków w przestrzeni i izolinie potencjału elektrycznego')
-# ax = fig.add_subplot(2, 1, 1, projection="3d")
 
 X, Y = np.meshgrid(np.linspace(0, 10, 100, endpoint=True), np.linspace(0, 10, 100, endpoint=True))
 
-Z = q[0] * k / ((X - wspolrzedne[0][0]) ** 2 + (Y - wspolrzedne[0][1]) ** 2+(0 - wspolrzedne[0][2]) ** 2) ** 0.5
+Z = q[0] * k / ((X - wspolrzedne[0][0]) ** 2 + (Y - wspolrzedne[0][1]) ** 2 + (0 - wspolrzedne[0][2]) ** 2) ** 0.5
 for i, Q in enumerate(q[1:]):
-    Z += Q * k / ((X - wspolrzedne[i][0]) ** 2 + (Y - wspolrzedne[i][1]) ** 2+(0 - wspolrzedne[i][2]) ** 2) ** 0.5
+    Z += Q * k / ((X - wspolrzedne[i][0]) ** 2 + (Y - wspolrzedne[i][1]) ** 2 + (0 - wspolrzedne[i][2]) ** 2) ** 0.5
 
-fig, ax = plt.subplots(constrained_layout=True, figsize=(6, 6))
-ax.set_aspect('equal', 'box')
+
+fig, ax = plt.subplots(1, 2, constrained_layout=True, figsize=(8, 4))
+ax[1].set_aspect('equal', 'box')
 levels = np.linspace(Z.min(), Z.max(), 20)
-cp = ax.contourf(X, Y, Z, levels=levels, cmap=cm.coolwarm)
+cp = ax[1].contourf(X, Y, Z, levels=levels, cmap=cm.spring)
 cbar = fig.colorbar(cp)
 cbar.ax.set_ylabel('electric potential')
-# ax2 = fig.add_subplot(1,2,1,projection="3d")
-# for o in range (len(q)):
-#     for x,y,z in wspolrzedne:
-#         if q[o] < 0:
-#             ax.scatter(x, y, z, c='blue')
-#         else:
-#             ax.scatter(x, y, z, c='red')
+ax[0] = fig.add_subplot(1, 2, 1, projection="3d")
+ax[0].scatter(x_ujemne, y_ujemne, z_ujemne, color="blue")
+ax[0].scatter(x_dodatnie, y_dodatnie, z_dodatnie, color="red")
+ax[0].set_title("Rozmieszczenie ładunków")
+
 
 plt.show()
